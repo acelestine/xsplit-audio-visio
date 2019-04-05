3a8e271ff2c304375d21d3b24bd8e5e81e470cd1
Daniel Cefram Ramirez <me@danielcefram.com>
2019-04-05 10:10:56 +0800

    wip: color up the bars

--- src/visualizers/Bars/visualizer.ts
+++ src/visualizers/Bars/visualizer.ts
@@ -6,7 +6,7 @@ let audioCtx = new AudioContext();
 let canvas: any;
 let ctx: any;
 
-function render(audio = 'default', sensitivity = 5) {
+function render({ audio = 'default', sensitivity = 5, color = '#F00' }) {
   navigator.mediaDevices
     .getUserMedia({
       audio: {
@@ -48,11 +48,12 @@ function render(audio = 'default', sensitivity = 5) {
         for (let i = 0; i < bufferLength; i++) {
           barHeight = dataArray[i];
 
-          const r = barHeight + 25 * (i / bufferLength);
-          const g = 250 * (i / bufferLength);
-          const b = 50;
+          // const r = barHeight + 25 * (i / bufferLength);
+          // const g = 250 * (i / bufferLength);
+          // const b = 50;
 
-          ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+          // ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+          ctx.fillStyle = color;
           ctx.fillRect(
             x,
             HEIGHT - barHeight * sensitivity,
@@ -78,17 +79,17 @@ function computeSensitivity(value: number) {
 }
 
 function handlePropsChange({ detail }: any) {
-  const { audio, sensitivity } = detail;
+  const { audio, sensitivity, color } = detail;
 
   if (canvas) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 
-  render(audio, computeSensitivity(sensitivity));
+  render({ audio, sensitivity: computeSensitivity(sensitivity), color });
 }
 
 export default function(obj: any) {
-  const { audio, sensitivity = 50 } = obj;
+  const { audio, sensitivity = 50, color = '#F00' } = obj;
 
   canvas = document.getElementById('canvas');
   canvas.width = window.innerWidth;
@@ -97,7 +98,7 @@ export default function(obj: any) {
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   document.addEventListener('props-change', handlePropsChange);
-  render(audio, computeSensitivity(sensitivity));
+  render({ audio, sensitivity: computeSensitivity(sensitivity), color });
 
   return () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
